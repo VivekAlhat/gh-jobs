aa5ffb49c9388e4e740deb54225da94e65f1f1b6
VivekAlhat <vivekalhat14@outlook.com>
2020-11-27 21:47:39 +0530

    gh-jobs backend

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const Axios = require("axios");
+const morgan = require("morgan");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(morgan("tiny"));
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`Server is up and running on ${port}`));
+
+app.post("/", (req, res) => {
+  const URI = `https://jobs.github.com/positions.json?description=${req.body.job}&location=${req.body.location}`;
+
+  Axios.default
+    .get(URI)
+    .then((result) => res.json(result.data))
+    .catch((err) => res.status(400).json(err));
+});
