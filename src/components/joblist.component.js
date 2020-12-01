e21fe5ae1db4e20d25ffc5c78625265543f06c27
VivekAlhat <vivekalhat14@outlook.com>
2020-12-02 01:48:38 +0530

    Create React Frontend

---
+++ src/components/joblist.component.js
@@ -0,0 +1,23 @@
+const JobList = (props) => {
+  return (
+    <li className="list-group-item py-3">
+      <div className="job-body">
+        <h4 className="card-title" style={{ fontSize: "1.2rem" }}>
+          {props.title}
+        </h4>
+        <h5 className="company-name">{props.company}</h5>
+        <p className="card-text">{props.description}</p>
+        <a
+          href={props.apply}
+          target="_blank"
+          className="btn btn-primary btn-sm"
+          rel="noreferrer"
+        >
+          Apply Now
+        </a>
+      </div>
+    </li>
+  );
+};
+
+export default JobList;
---
+++ src/components/search.component.js
@@ -0,0 +1,135 @@
+import React, { Component } from "react";
+import Axios from "axios";
+import JobCard from "./jobcard.component";
+import JobList from "./joblist.component";
+export default class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      job: "",
+      location: "",
+      searchResult: [],
+      dataReturned: true,
+      viewas: "",
+    };
+  }
+
+  onJobValueChange = (e) => {
+    this.setState({
+      job: e.target.value,
+    });
+  };
+
+  onLocationValueChange = (e) => {
+    this.setState({
+      location: e.target.value,
+    });
+  };
+
+  onViewOptionChange = (e) => {
+    this.setState({
+      viewas: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const jobObject = {
+      job: this.state.job,
+      location: this.state.location,
+    };
+
+    Axios.post("http://localhost:8000/jobs", jobObject)
+      .then((res) => {
+        if (parseInt(res.headers["content-length"]) === 2) {
+          this.setState({ searchResult: res.data, dataReturned: false });
+        } else {
+          this.setState({ searchResult: res.data, dataReturned: true });
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  render() {
+    return (
+      <div className="container px-5">
+        <h5 className="header-tag text-center mb-4">
+          Find your next job with Github!
+        </h5>
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="e.g Python"
+              value={this.state.job}
+              onChange={this.onJobValueChange}
+              required={true}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="e.g New York"
+              value={this.state.location}
+              onChange={this.onLocationValueChange}
+              required={true}
+            />
+          </div>
+          <button type="submit" className="btn btn-outline-primary">
+            Search
+          </button>
+        </form>
+        <hr
+          className="mt-3"
+          style={{ borderTop: "1px solid #839b97", width: "100%" }}
+        />
+
+        {this.state.dataReturned === false && (
+          <p className="text-center">No data available for given parameters.</p>
+        )}
+
+        {this.state.searchResult.length > 0 && (
+          <div
+            className="form-group radio-group px-3"
+            onChange={this.onViewOptionChange}
+          >
+            <label className="radio-inline mr-2">
+              <input type="radio" value="card" name="viewas" /> Card
+            </label>
+            <label className="radio-inline ml-2">
+              <input type="radio" value="list" name="viewas" /> List
+            </label>
+          </div>
+        )}
+        {this.state.viewas === "card" ? (
+          <div className="row mt-3">
+            {this.state.searchResult.map((e) => (
+              <JobCard
+                key={e.id}
+                logo={e.company_logo}
+                title={e.title}
+                company={e.company}
+                description={e.type}
+                apply={e.url}
+              />
+            ))}
+          </div>
+        ) : (
+          <ul className="list-group list-group-flush mt-3">
+            {this.state.searchResult.map((e) => (
+              <JobList
+                key={e.id}
+                title={e.title}
+                company={e.company}
+                description={e.type}
+                apply={e.url}
+              />
+            ))}
+          </ul>
+        )}
+      </div>
+    );
+  }
+}
